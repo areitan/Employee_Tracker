b56ee6ee96706efdcae4b48d9e1a89bf54183c63
April Reitan <45362322+areitan@users.noreply.github.com>
2022-05-13 20:09:01 -0700

    adding some inquirer prompts

--- server.js
+++ server.js
@@ -22,13 +22,53 @@ const db = mysql.createConnection(
   console.log(`Connected to employee_db database.`)
 );
 
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+function menu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"]
+      },
+      .then((data) => {
+        if (data.choice === "Add Department")
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "name",
+                message: "What is the department name?",
+              }]),
+              app.get('/', (req, res) => {
+                db.query('INSERT "name" INTO department', function (err, results) {
+                  console.log(results);
+                });
+              })
+        })])
+}
+
+// app.get('/', (req, res) => {
+//   db.query('SELECT * FROM favorite_books', function (err, results) {
+//     console.log(results);
+//   });
+// });
+
+// app.get('/api/terms', (req, res) => res.json(termData));
 
 
+// // Query database
+// db.query('SELECT * FROM course_names', function (err, results) {
+//   console.log(results);
+// });
 
-
-
-
-
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
 
 
 
