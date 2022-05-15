d2cb1b081b23b1a434086022415e59c3c7156110
April Reitan <45362322+areitan@users.noreply.github.com>
2022-05-14 22:33:18 -0700

    adding server.js code and removing extra comments and lines

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-// Import and require express
+// Imports and requires
 const express = require("express");
-// Import and require mysql2
 const mysql = require("mysql2");
-// require inquirer
 const inquirer = require("inquirer");
-// Import and console.table
 const consoleTable = require("console.table");
-
-
 const { init } = require("express/lib/application");
 
-// adding port and app express
+// port and app express
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -56,7 +51,7 @@ function menu() {
           name: "name",
           message: "What is the department name?",
         }]),
-      app.post("/db", ({ body }, res) => {
+      app.post("/api", ({ body }, res) => {
         const sql = `INSERT INTO department (name)
             VALUES (?)`;
         const params = [body.movie_name];
@@ -96,7 +91,7 @@ function menu() {
           message: "What is the department this role belong to?",
         }
       ]),
-      app.post("/db", ({ body }, res) => {
+      app.post("/api", ({ body }, res) => {
         const sql = `INSERT INTO role (title, salary, department_id) 
             VALUES (?,?,?)`;
         const params = [body.movie_name];
@@ -141,9 +136,9 @@ function menu() {
           message: "What is the employee's manager's id?",
         }
       ]),
-      app.post("/db", ({ body }, res) => {
-        const sql = `INSERT INTO employee (title, salary, department_id) VALUES (?,?,?)
-            VALUES (?)`;
+      app.post("/api", ({ body }, res) => {
+        const sql = `INSERT INTO employee (first_name, last_name, role_id,manager_id)
+            VALUES (?,?,?,?)`;
         const params = [body.movie_name];
         db.query(sql, params, (err, result) => {
           if (err) {
@@ -160,72 +155,110 @@ function menu() {
   menu();
 });
 
+// update an Employee Role
+.then((data) => {
+  if (data.choice === "Update Employee Role") {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "id",
+          message: "What is the employee's id?",
+        },
+        {
+          type: "input",
+          name: "role_id",
+          message: "What is the employee's new role id?",
+        },
+      ]),
+      app.put("/api/employee/:id", (req, res) => {
+        const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+        const params = [req.body.review, req.params.id];
+
+        db.query(sql, params, (err, result) => {
+          if (err) {
+            res.status(400).json({ error: err.message });
+          } else if (!result.affectedRows) {
+            res.json({
+              message: "Employee not found",
+            });
+          } else {
+            res.json({
+              message: "success",
+              data: req.body,
+              changes: result.affectedRows,
+            });
+          }
+        });
+        menu();
+      });
+
 // View department Table
 .then((data) => {
-  if (data.choice === "View All Departments") {
-    app.get('/db', (req, res) => {
-      const sql = `'SELECT id, name AS Department FROM department;`;
-      db.query(sql, (err, rows) => {
-        if (err) {
-          res.status(500).json({ error: err.message });
-          return;
+        if (data.choice === "View All Departments") {
+          app.get('/api', (req, res) => {
+            const sql = `'SELECT id, name AS Department FROM department;`;
+            db.query(sql, (err, rows) => {
+              if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+              }
+              res.json({
+                message: "success",
+                data: rows,
+              });
+            })
+          });
         }
-        res.json({
-          message: "success",
-          data: rows,
-        });
-      })
-    });
-  }
-  menu();
-});
+        menu();
+      });
 
 // View role Table
 .then((data) => {
-  if (data.choice === "View All Roles") {
-    app.get('/db', (req, res) => {
-      const sql = `'SELECT * FROM role;`;
-      db.query(sql, (err, rows) => {
-        if (err) {
-          res.status(500).json({ error: err.message });
-          return;
+        if (data.choice === "View All Roles") {
+          app.get('/api', (req, res) => {
+            const sql = `'SELECT * FROM role;`;
+            db.query(sql, (err, rows) => {
+              if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+              }
+              res.json({
+                message: "success",
+                data: rows,
+              });
+            })
+          });
         }
-        res.json({
-          message: "success",
-          data: rows,
-        });
-      })
-    });
-  }
-  menu();
-});
+        menu();
+      });
 
 // View employees Table
 .then((data) => {
-  if (data.choice === "View All Employees") {
-    app.get('/db', (req, res) => {
-      const sql = `'SELECT * FROM employee;`;
-      db.query(sql, (err, rows) => {
-        if (err) {
-          res.status(500).json({ error: err.message });
-          return;
+        if (data.choice === "View All Employees") {
+          app.get('/api', (req, res) => {
+            const sql = `'SELECT * FROM employee;`;
+            db.query(sql, (err, rows) => {
+              if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+              }
+              res.json({
+                message: "success",
+                data: rows,
+              });
+            })
+          });
         }
-        res.json({
-          message: "success",
-          data: rows,
-        });
-      })
+        menu();
+      });
+
+    function init() {
+      menu();
+    };
+
+    init();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
-  }
-  menu();
-});
-
-function init() {
-  menu();
-};
-
-init();
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
