a70dc045ed3eb19d61aaef89352b8823a0c6b4c4
April Reitan <45362322+areitan@users.noreply.github.com>
2022-05-14 21:07:07 -0700

    removing index.js and adding inquirer to server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+// Import and require express
 const express = require("express");
 // Import and require mysql2
 const mysql = require("mysql2");
+// require inquirer
+const inquirer = require("inquirer");
+// require inquirer
+const fs = require("fs");
+const { init } = require("express/lib/application");
 
+// adding port and app express
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -22,28 +29,77 @@ const db = mysql.createConnection(
   console.log(`Connected to employee_db database.`)
 );
 
+// Menu function using inquirer
+function menu() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "choice",
+                message: "What would you like to do?",
+                choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"]
+            },])
+    return data;
+};
 
+// Add a department
+function newDepartment() {
+    if (data.choice === "Add Department")
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "What is the department name?",
+                }]),
+            app.get('/', (req, res) => {
+                db.query('INSERT "name" INTO department', function (err, results) {
+                    console.log(results);
+                });
+                menu();
+            })
+};
 
-// app.get('/', (req, res) => {
-//   db.query('SELECT * FROM favorite_books', function (err, results) {
-//     console.log(results);
-//   });
-// });
+// View department Table
+function viewDepartments() {
+    if (data.choice === "View All Departments")
+        app.get('/', (req, res) => {
+            db.query('SELECT * FROM department', function (err, results) {
+                console.log(results);
+            });
+            menu();
+        })
+};
 
-// app.get('/api/terms', (req, res) => res.json(termData));
+// View role Table
+function viewRoles() {
+    if (data.choice === "View All Roles")
+        app.get('/', (req, res) => {
+            db.query('SELECT * FROM role', function (err, results) {
+                console.log(results);
+            });
+            menu();
+        })
+};
 
+// View employees Table
+function viewEmployees() {
+    if (data.choice === "View All Employees")
+        app.get('/', (req, res) => {
+            db.query('SELECT * FROM employee', function (err, results) {
+                console.log(results);
+            });
+            menu();
+        })
+};
 
-// // Query database
-// db.query('SELECT * FROM course_names', function (err, results) {
-//   console.log(results);
-// });
+// 
 
-// // Default response for any other request (Not Found)
-// app.use((req, res) => {
-//   res.status(404).end();
-// });
+init() {
+  menu();
+};
 
-
+init();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
